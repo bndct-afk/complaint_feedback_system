0aa509f5e5df8f3a5637a973aa888a3a380ec701
bndct-afk <armianbenedict@gmail.com>
2026-05-13 10:29:22 +0800

    Update login.php

--- login.php
+++ login.php
@@ -1,9 +1,8 @@
 <?php
 include 'config/database.php';
 include 'includes/session.php';
+include 'includes/header.php';
 ?>
-
-<?php include 'includes/header.php'; ?>
 
 <div class="container mt-5" style="max-width:500px;">
 
