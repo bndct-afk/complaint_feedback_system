89da88d4b461dcbdefda7bf48d265c91f5f7d34d
bndct-afk <armianbenedict@gmail.com>
2026-05-13 10:14:33 +0800

    Refactor database connection to use environment variables

--- config/database.php
+++ config/database.php
@@ -1,14 +1,14 @@
 <?php
 
-$host = "localhost";
-$username = "root";
-$password = "";
-$database = "complaint_system";
+$host = getenv("DB_HOST");
+$user = getenv("DB_USER");
+$pass = getenv("DB_PASS");
+$db   = getenv("DB_NAME");
+$port = getenv("DB_PORT");
 
-$conn = mysqli_connect($host, $username, $password, $database);
+$conn = mysqli_connect($host, $user, $pass, $db, $port);
 
 if (!$conn) {
-    die("Connection failed: " . mysqli_connect_error());
+    die("Database connection failed: " . mysqli_connect_error());
 }
-
 ?>
